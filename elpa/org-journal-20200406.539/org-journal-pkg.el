aa202a8f047197a220e9f0179fc1cb5870684445
NinjaCursor <supercursor22@gmail.com>
2020-04-16 21:46:25 -0400

    asdf

---
+++ elpa/org-journal-20200406.539/org-journal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-journal" "20200406.539" "a simple org-mode based journaling mode" '((emacs "25.1")) :commit "9abafc1960488e6687328db5f8c6019930a86d23" :authors '(("Bastian Bechtold") ("Christian Schwarzgruber")) :maintainer '("Bastian Bechtold") :url "http://github.com/bastibe/org-journal")
