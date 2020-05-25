e278fb4195d1d3877a0f8f26205154bcfd20bf44
NinjaCursor <supercursor22@gmail.com>
2020-05-24 20:03:54 -0400

    Added code to prevent backups and saves of encrypted files

--- init.el
+++ init.el
@@ -11,8 +11,12 @@
 			 ("magit" . "")
 			 ))
 
+
+(require 'epa-file)
+(epa-file-enable)
 					; activate all the packages (in particular autoloads)
 (package-initialize)
+
 
 					; fetch the list of packages available 
 (unless package-archive-contents
@@ -36,21 +40,88 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#ffffff" "#37ffff" "#e074e3" "#3732ff" "#ffff0b" "#37ff3c" "#ff400b" "#848088"])
- '(custom-enabled-themes (quote (leuven-dark)))
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#93E0E3")
+ '(cua-normal-cursor-color "#DCDCCC")
+ '(cua-overwrite-cursor-color "#F0DFAF")
+ '(cua-read-only-cursor-color "#7F9F7F")
+ '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "4e0c46bacfa18716370f52f4e1acda19ddeced16caac66afc33fa0d0161df111" "5a28123387ad126d39f4f1200f953fe6a3a5397c35efc9628be572a1a167ebe0" "f3b2a32914eebbc95b08f04d4377ed8b51205037082a5f20686c0c1aad2cce89" "5f1bd7f67dc1598977e69c6a0aed3c926f49581fdf395a6246f9bc1df86cb030" "3f5f69bfa958dcf04066ab2661eb2698252c0e40b8e61104e3162e341cee1eb9" default)))
+    ("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "4e0c46bacfa18716370f52f4e1acda19ddeced16caac66afc33fa0d0161df111" "5a28123387ad126d39f4f1200f953fe6a3a5397c35efc9628be572a1a167ebe0" "f3b2a32914eebbc95b08f04d4377ed8b51205037082a5f20686c0c1aad2cce89" "5f1bd7f67dc1598977e69c6a0aed3c926f49581fdf395a6246f9bc1df86cb030" "3f5f69bfa958dcf04066ab2661eb2698252c0e40b8e61104e3162e341cee1eb9" default)))
+ '(fci-rule-color "#4F4F4F")
+ '(highlight-changes-colors (quote ("#DC8CC3" "#bbb0cb")))
+ '(highlight-symbol-colors
+   (quote
+    ("#680f63eb5998" "#54db645064d0" "#6097535f5322" "#5c2859a95fa1" "#4ede55f24ea4" "#64dd5979525e" "#530060d16157")))
+ '(highlight-symbol-foreground-color "#FFFFEF")
+ '(highlight-tail-colors
+   (quote
+    (("#4F4F4F" . 0)
+     ("#488249" . 20)
+     ("#5dacaf" . 30)
+     ("#57a2a4" . 50)
+     ("#b6a576" . 60)
+     ("#ac7b5a" . 70)
+     ("#aa5790" . 85)
+     ("#4F4F4F" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#b6a576" "#ac7b5a" "#9f5c5c" "#aa5790" "#85749c" "#57a2a4" "#5dacaf" "#488249")))
+ '(hl-fg-colors
+   (quote
+    ("#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F")))
+ '(hl-paren-colors (quote ("#93E0E3" "#F0DFAF" "#8CD0D3" "#bbb0cb" "#7F9F7F")))
  '(hl-sexp-background-color "#33323e")
+ '(lsp-ui-doc-border "#FFFFEF")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#488249" "#95d291" "#57a2a4" "#93E0E3" "#DC8CC3" "#bbb0cb")))
  '(org-capture-templates
    (quote
     (("b" "For recording those lightbulb moments" entry
       (file "~/org/brainstorm.org")
       "" :prepend t))) t)
- '(org-journal-dir "~/org-journal/")
+ '(org-journal-dir "~/daily_tasks/")
  '(org-journal-file-format "%m.%d.%Y.org")
  '(package-selected-packages
    (quote
-    (solarized-theme magit popup-complete auctex-lua org-edit-latex org-re-reveal-ref org-ref pdf-tools eww-lnum use-package eyebrowse org-journal dracula-theme leuven-theme))))
+    (solarized-theme magit popup-complete auctex-lua org-edit-latex org-re-reveal-ref org-ref pdf-tools eww-lnum use-package eyebrowse org-journal dracula-theme leuven-theme)))
+ '(pos-tip-background-color "#4F4F4F")
+ '(pos-tip-foreground-color "#FFFFEF")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#7F9F7F" "#4F4F4F" 0.2))
+ '(term-default-bg-color "#3F3F3F")
+ '(term-default-fg-color "#DCDCCC")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#CC9393")
+     (40 . "#df51b97ca1ae")
+     (60 . "#e83dcc9aa8b1")
+     (80 . "#F0DFAF")
+     (100 . "#cadbca369f51")
+     (120 . "#b7fbbf79973e")
+     (140 . "#a52cb4cc8f3f")
+     (160 . "#9260aa2d8754")
+     (180 . "#7F9F7F")
+     (200 . "#87dbb4dba003")
+     (220 . "#8b6ebfadb0a1")
+     (240 . "#8e96ca9fc17c")
+     (260 . "#914ed5b0d293")
+     (280 . "#93E0E3")
+     (300 . "#90c5da6cdd6f")
+     (320 . "#8f5dd735da39")
+     (340 . "#8df4d401d704")
+     (360 . "#8CD0D3"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#3F3F3F" "#4F4F4F" "#9f5c5c" "#CC9393" "#488249" "#7F9F7F" "#b6a576" "#F0DFAF" "#57a2a4" "#8CD0D3" "#aa5790" "#DC8CC3" "#5dacaf" "#93E0E3" "#DCDCCC" "#6F6F6F")))
+ '(xterm-color-names
+   ["#4F4F4F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#fffff6"])
+ '(xterm-color-names-bright
+   ["#3F3F3F" "#DFAF8F" "#878777" "#6F6F6F" "#DCDCCC" "#bbb0cb" "#FFFFEF" "#FFFFFD"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -337,6 +408,37 @@ PRIORITY may be one of the characters ?A, ?B, or ?C."
 
 ;; change backups directory of files
 (setq backup-directory-alist '(("." . "~/.emacs-backup")))
+
+(define-minor-mode sensitive-mode
+  "For sensitive files like password lists.
+It disables backup creation and auto saving.
+
+With no argument, this command toggles the mode.
+Non-null prefix argument turns on the mode.
+Null prefix argument turns off the mode."
+  ;; The initial value.
+  nil
+  ;; The indicator for the mode line.
+  " Sensitive"
+  ;; The minor mode bindings.
+  nil
+  (if (symbol-value sensitive-mode)
+      (progn
+	;; disable backups
+	(set (make-local-variable 'backup-inhibited) t)	
+	;; disable auto-save
+	(if auto-save-default
+	    (auto-save-mode -1)))
+    ;resort to default value of backup-inhibited
+    (kill-local-variable 'backup-inhibited)
+    ;resort to default auto save setting
+    (if auto-save-default
+	(auto-save-mode 1))))
+
+
+(setq auto-mode-alist
+ (append '(("\\.gpg$" . sensitive-mode))
+               auto-mode-alist))
 
 
 ;; save-hook-function
