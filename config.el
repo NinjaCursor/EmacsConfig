47c81f72dad0c8904d8e15993fa26763a3a1c2f7
Jacob Brink <jacobpeterbrink@gmail.com>
2020-09-11 12:16:32 -0400

    Migrated eyebrowse configuration to config.org

--- config.el
+++ config.el
@@ -215,3 +215,13 @@ Null prefix argument turns off the mode."
 	))
 
 (define-key global-map (kbd "C-c g") 'magit-status)
+
+(use-package eyebrowse
+  :diminish eyebrowse-mode
+  :config (progn
+	    (define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+	    (define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+	    (define-key eyebrowse-mode-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+	    (define-key eyebrowse-mode-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
+	    (eyebrowse-mode t)
+	    (setq eyebrowse-new-workspace t)))
